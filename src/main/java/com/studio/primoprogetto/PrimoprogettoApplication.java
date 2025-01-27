51550d8f30c856bfcffd82ab4f3fe08385ed05a3
leog18 <leonardogalluzzi824@gmail.com>
2025-01-27 19:00:52 +0100

    init

---
+++ src/main/java/com/studio/primoprogetto/PrimoprogettoApplication.java
@@ -0,0 +1,13 @@
+package com.studio.primoprogetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimoprogettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimoprogettoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studio/primoprogetto/PrimoprogettoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studio.primoprogetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimoprogettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

6737c01d763d56da07ac68d04b5ab4ca55af4b39
leog18 <leonardogalluzzi824@gmail.com>
2025-01-27 20:38:08 +0100

    modifica controller aggiunta azioni get put delete

--- src/main/java/com/studio/primoprogetto/controller/ProdottoController.java
+++ src/main/java/com/studio/primoprogetto/controller/ProdottoController.java
@@ -6,9 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -27,4 +25,21 @@ public class ProdottoController {
         log.info("Controller - addNewProduct end");
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
+
+    @GetMapping  ("/product")
+    public ResponseEntity<Object> getProduct(){
+        log.info("Controller - getProduct start with -> {}");
+        log.info("Controller - getProduct end");
+        return new ResponseEntity<>(HttpStatus.CREATED);@
+    }
+    @PutMapping  ("/product")
+    public ResponseEntity<Object> putProduct(){
+        log.info("Controller - putProduct start with -> {}");
+        log.info("Controller - putProduct end");
+        return new ResponseEntity<>(HttpStatus.CREATED);
 }
+@DeleteMapping  ("/product")
+    public ResponseEntity<Object> deleteProduct(){
+        log.info("Controller - deleteProduct start with -> {}");
+        log.info("Controller - deleteProduct end");
+        return new ResponseEntity<>(HttpStatus.CREATED);
